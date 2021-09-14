f512215bf2b14625757759f0029530d298625d40
MRensen <markrensen@hotmail.com>
2021-09-15 00:13:48 +0200

    switched lowdeck and spimpeldeck

--- start/novi/Main.java
+++ start/novi/Main.java
@@ -2,15 +2,16 @@ package novi;
 
 import java.util.Scanner;
 
+import novi.blackjackVariant.blackjack.LowDeckBlackjack;
 import novi.higherlower.HigherLowerGame;
-import novi.blackjack.BlackjackGame;
+import novi.blackjackVariant.blackjack.BlackjackGame;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
 
-        BlackjackGame game = new BlackjackGame(inputScanner);
+        BlackjackGame game = new LowDeckBlackjack(inputScanner);
 
 //        HigherLowerGame game = new HigherLowerGame(inputScanner);
 
--- start/novi/blackjackVariant/blackjack/LowDeck.java
+++ start/novi/blackjackVariant/blackjack/LowDeck.java
@@ -1,7 +1,34 @@
 package novi.blackjackVariant.blackjack;
 
+import java.util.ArrayList;
+
 public class LowDeck extends Deck{
      public LowDeck(){
-         super();
+
+         cards= new ArrayList<>();
+
+         final CardSuit[] suits;
+         final CardValue[] values;
+         suits = new CardSuit[] {
+                 new CardSuit("diamonds", '\u2666', "red"),
+                 new CardSuit("spades", '\u2660', "black"),
+                 new CardSuit("hearts", '\u2665',"red"),
+                 new CardSuit("clubs",'\u2663',"red")
+         };
+         values = new CardValue[] {
+                 new CardValue("two", 2),
+                 new CardValue("three", 3),
+                 new CardValue("four", 4),
+                 new CardValue("five", 5),
+                 new CardValue("six", 6)
+         };
+         for(CardSuit suit : suits){
+             for(CardValue value: values){
+                 cards.add(new Card(suit, value));
+                 cards.add(new Card(suit, value));
+                 cards.add(new Card(suit, value));
+             }
+         }
+
      }
 }
